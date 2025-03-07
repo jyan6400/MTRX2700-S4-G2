594a0ffaf9241e8be627452163a02c9fe24170df
sjzmiaomiao <149314788+sjzmiaomiao@users.noreply.github.com>
2025-03-08 01:56:02 +1100

    Change it again to meet the assignment requirements
    R2 can be used to set the case conversion

--- 1.3.2/Src/a.s
+++ 1.3.2/Src/a.s
@@ -7,58 +7,72 @@
 .data
 @ define variables
 
-string_buffer: .asciz "replace this text replace this text" @ Define a null-terminated string
-ascii_string: .asciz "Hello world!" @ Define a null-terminated string
+string_buffer: .asciz "replace this text replace this text" @ Define a store string
+ascii_string: .asciz "Hello [] World!" @ Define a import string
 
 
 .text
 @ define text
-    BL letters_convertion
-
 @ this is the entry function called from the startup file
 main:
 
-	LDR R1, =ascii_string  @ the address of the string
-	LDR R2, =string_buffer  @ the address of the string stored
+	LDR R0, =ascii_string  @ the address of the string
+	LDR R1, =string_buffer  @ the address of the string stored
 	LDR R3, =0x00 	@ counter to the current place in the string
-
+	MOV R2, #1      @if R2=1 upperconver, R2=0 lowerconver
+    BL letters_convertion
 
 letters_convertion:
 
-    LDRB R4, [R1, R3]   @ loading Ascii string
-    CMP R4, #0          @ Detect the NULL of the strings
-    BEQ finished_loop   @ if touch the NULL, finished——loop
-
+    LDRB R4, [R0, R3]   @ loading Ascii string
+    CMP R4, #0          @ detect the NULL of the strings
+    BEQ finished_loop   @ if touch the NULL, finished_loop
 
     CMP R4, #65         @ compare the R4 Ascii < 65 (nonalphabetic)
     BLT store           @ if < 65 skip to store
 
-
-    CMP R4, #90         @ compare the R4 Ascii <= 90 (Upperalphabetic)
-    BLE Lowercase_convertion    @ if <= 90 skip to Lowercase_convertion
-
+    CMP R4, #122        @ compare the R4 Ascii <= 90 (alphabetic)
+    BLE Convertion      @ if <= 90 skip to Convertion
 
     CMP R4, #122        @ compare the R4 Ascii > 122 (nonalphabetic)
     BGT store           @ if > 122 skip to store
 
-    SUB R4, #32     @ convert the lowercase to uppercase
-    B store         @ store strings
+
+    Convertion:
+
+    CMP R2,#1                   @ if R2=1 is true convert Uppercase
+    BEQ Uppercase_convertion    @ skip to Uppercase
+
+    CMP R2,#0                   @ if R2=1 is true convert Lowercase
+    BEQ  Lowercase_convertion   @ skip to Lowercase
+
+
+    Uppercase_convertion:
+
+    CMP R4, #96         @compare the R4 Ascii <= 96
+    BLE store           @ if <= 96 skip to store
+
+    SUB R4, #32         @ convert the lowercase to uppercase
+    B store             @ store strings
+
 
     Lowercase_convertion:
-    ADD R4, #32    @ convert the uppercase to lowercase
+
+    CMP R4, #91         @compare the R4 Ascii >= 91
+    BGE store           @ if >= 91 skip to store
+
+
+    ADD R4, #32         @ convert the uppercase to lowercase
+    B store             @ store strings
+
+
 
     store:
-    STRB R4, [R2, R3]   @ storing the converted strings
+    STRB R4, [R1, R3]   @ storing the converted strings
 
-	ADD R3, #1  @ increment the offset R2
+	ADD R3, #1          @ increment the offset R2
 
     B letters_convertion
 
 finished_loop:
 	BX LR  @ loop to the next byte
-
-
-
-finished_everything:
-
-	B finished_everything 	@ infinite loop here
