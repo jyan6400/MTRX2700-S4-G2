cef570da46b7c1bbc8a9cf37e5125886a493f83e
oscareverett <oceverett@yahoo.com.au>
2025-03-23 17:53:01 +1100

    Update mcu1.s

--- Weekly Labs/1.7.2/mcu1.s
+++ Weekly Labs/1.7.2/mcu1.s
@@ -14,16 +14,16 @@ buffer_index:     .word 0
 main:
     BL initialise_power
     BL enable_peripheral_clocks
-    BL enable_discovery_board
+    BL initialise_discovery_board
     BL enable_uart        @ USART1 (PC)
     BL enable_uart2       @ USART2 (to MCU2)
 
     LDR R4, =input_buffer
     LDR R5, =buffer_index
-    MOV R6, #0x21         @ '!' character
+    MOV R6, #0x21          @ '!' terminator
 
 read_loop:
-    LDR R0, =UART
+    LDR R0, =UART          @ USART1
     LDR R1, [R0, #USART_ISR]
     TST R1, #(1 << UART_RXNE)
     BEQ read_loop
@@ -41,26 +41,26 @@ read_loop:
 check_palindrome:
     LDR R0, =input_buffer
     LDR R5, =buffer_index
-    LDR R4, [R5]            @ Get length
-    MOV R1, #0
-    LDR R2, =reversed_buffer
+    LDR R4, [R5]           @ Length including '!'
+    SUB R4, R4, #2         @ Exclude '!' from check
 
-    @ Reverse the buffer
-    SUB R4, R4, #2          @ Exclude '!' at end for palindrome check
+    LDR R1, =reversed_buffer
+    MOV R5, #0
+
 reverse_loop:
     CMP R4, #0
     BLT done_reverse
     LDR R3, =input_buffer
     ADD R3, R3, R4
-    LDRB R7, [R3]
-    STRB R7, [R2, R1]
-    ADD R1, R1, #1
+    LDRB R2, [R3]
+    STRB R2, [R1, R5]
+    ADD R5, R5, #1
     SUB R4, R4, #1
     B reverse_loop
 
 done_reverse:
     MOV R3, #0
-    STRB R3, [R2, R1]
+    STRB R3, [R1, R5]      @ Null-terminate reversed string
 
     LDR R0, =input_buffer
     LDR R1, =reversed_buffer
@@ -68,24 +68,24 @@ done_reverse:
 compare_loop:
     LDRB R2, [R0], #1
     LDRB R3, [R1], #1
-    CMP R2, #0x21        @ Stop comparing at '!'
+    CMP R2, #0x21          @ Stop comparing at '!'
     BEQ is_palindrome
     CMP R2, R3
     BNE forward_message
     B compare_loop
 
 is_palindrome:
+    @ Light up PE12–PE15
     LDR R0, =GPIOE
     LDR R3, [R0, #0x14]
-    ORR R3, R3, #(1 << 9 | 1 << 11 | 1 << 13 | 1 << 15)
+    ORR R3, R3, #(1 << 12 | 1 << 13 | 1 << 14 | 1 << 15)
     STR R3, [R0, #0x14]
 
 forward_message:
-    @ Send entire buffer (including '!')
     LDR R4, =input_buffer
     LDR R5, =buffer_index
-    LDR R6, [R5]             @ Length incl. '!'
-    MOV R7, #0               @ Index
+    LDR R6, [R5]            @ Total message length
+    MOV R7, #0
 
 send_loop:
     CMP R7, R6
@@ -100,10 +100,9 @@ wait_tx_ready:
     TST R1, #(1 << UART_TXE)
     BEQ wait_tx_ready
     STRB R2, [R0, #USART_TDR]
-
     B send_loop
 
 reset:
     MOV R2, #0
-    STR R2, [R5]           @ Reset buffer index
+    STR R2, [R5]            @ Reset buffer index
     B read_loop
