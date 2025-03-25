5c43f1baf1b2ba4a74284564b438c438974cf0bb
oscareverett <oceverett@yahoo.com.au>
2025-03-25 20:27:20 +1100

    Update 1.4.2a.s

--- Weekly Labs/1.4.2/1.4.2a.s
+++ Weekly Labs/1.4.2/1.4.2a.s
@@ -20,16 +20,14 @@ main:
     @ Once the clocks are started, need to initialise the discovery board I/O
     BL initialise_discovery_board
 
-    @ Branch to function to set LED pattern
-    B bitmask
-
-
-
+@ Function to apply bitmask
 bitmask:
-	@ Bitmask setting LEDs to given pattern
-	LDR R0, =0b01110001
+	@ Bitmask setting LEDs to given pattern (First 4 on)
+	LDR R0, =0b00001111
+ 
 	@Loading GPIOE into R1
 	LDR R1, =GPIOE
-	@ Store R0 to ODR (bits 8-15)
-    STRB R0, [R1, #ODR + 1]
+ 
+	@ Store bitmask into bits 8-15
+    	STRB R0, [R1, #ODR + 1]
 	BX LR
