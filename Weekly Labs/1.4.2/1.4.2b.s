a5b2108009ee0fa1df14aae56136a05455615830
oscareverett <oceverett@yahoo.com.au>
2025-03-25 21:08:55 +1100

    Update 1.4.2b.s

--- Weekly Labs/1.4.2/1.4.2b.s
+++ Weekly Labs/1.4.2/1.4.2b.s
@@ -28,33 +28,33 @@ program_loop:
     STRB R4, [R0, #ODR + 1]  @ Store R4 to bits 8-15
 
     @ Read the input button state
-    LDR R0, =GPIOA    @ Port for the input button
-    LDRB R1, [R0, #IDR]@ load the lowest byte from the input data register port A
-    ANDS R1, #0x01    @ Look only at bit 0 (input value of PA0)
-    BNE pressed       @ Loop until the button is pressed
+    LDR R0, =GPIOA    	@ Port for the input button
+    LDRB R1, [R0, #IDR]	@ load the lowest byte from the input data register port A
+    ANDS R1, #0x01    	@ Look only at bit 0 (input value of PA0)
+    BNE pressed       	@ Loop until the button is pressed
 
-    B program_loop    @ Repeat loop
+    B program_loop    	@ Repeat loop
 
 pressed:
     @ If all LEDs are off, turn on the first LED
     CMP R4, #0
-    BNE change_leds    @ If R4 is NOT zero, continue shifting LEDs
-    LDR R4, =0b00000001  @ If R4 is zero, start with the first LED
+    BNE change_leds    	@ If R4 is NOT zero, continue shifting LEDs
+    LDR R4, =0b00000001 @ If R4 is zero, start with the first LED
     B released
 
 change_leds:
-    LSL R4, #1  @ Shift left to turn on the next LED
-    ORR R4, #1  @ Keep previous LEDs on
+    LSL R4, #1  		@ Shift left to turn on the next LED
+    ORR R4, #1  		@ Keep previous LEDs on
 
     @ If all LEDs are on reset to 0 LEDs
     CMP R4, #0b11111111 @ Check if all LEDs are on
-    BLE released   @ If not then continue as normal
-    LDR R4, =0b00000000  @ If all are on then reset to 0 on
+    BLE released   		@ If not then continue as normal
+    LDR R4, =0b00000000 @ If all are on then reset to 0 on
 
 released:
     @ Wait for button to be released
     LDRB R1, [R0, #IDR] @ load the lowest byte from the input data register port A
-    ANDS R1, #0x01  @ Look only at bit 0 (input value of PA0)
-    BNE released  @ loop until button is unpressed
+    ANDS R1, #0x01  	@ Look only at bit 0 (input value of PA0)
+    BNE released  		@ loop until button is unpressed
 
-    B program_loop  @ Return to program_loop once button is unpressed
+    B program_loop  	@ Return to program_loop once button is unpressed
