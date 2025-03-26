1abfbc84c9c010a6926b8b761a578b29d4c38cc8
oscareverett <oceverett@yahoo.com.au>
2025-03-26 11:27:49 +1100

    Update mcu1.s

--- Weekly Labs/1.5.2/1.5.2e/mcu1.s
+++ Weekly Labs/1.5.2/1.5.2e/mcu1.s
@@ -7,24 +7,25 @@
 
 .text
 main:
+	@ Enbaling power, GPIO clocks and USARTs
     BL initialise_power
     BL enable_peripheral_clocks
-    BL enable_uart      @ USART1 (PC)
-    BL enable_uart2     @ USART2 (to MCU2)
+    BL enable_usart      		  @ USART1 connected to PC
+    BL enable_usart2     		  @ USART2 connected to MCU2
 
 forward_loop:
-    LDR R0, =UART
-    LDR R1, [R0, #USART_ISR]
-    TST R1, #(1 << UART_RXNE)
-    BEQ forward_loop
+    LDR R0, =UART				  @ USART1 base address
+    LDR R1, [R0, #USART_ISR]	  @ Load USART1 status
+    TST R1, #(1 << UART_RXNE)	  @ Check if ready to receive data
+    BEQ forward_loop			  @ If not ready then keep checking
 
     LDRB R2, [R0, #USART_RDR]     @ Read byte from PC
 
-    LDR R0, =UART2
+    LDR R0, =UART2				  @ USART2 base adress
 wait_tx_ready:
-    LDR R1, [R0, #USART_ISR]
-    TST R1, #(1 << UART_TXE)
-    BEQ wait_tx_ready
+    LDR R1, [R0, #USART_ISR]	  @ Load status
+    TST R1, #(1 << UART_TXE)	  @ Check if ready to transmit
+    BEQ wait_tx_ready			  @ Loop if not ready
 
     STRB R2, [R0, #USART_TDR]     @ Send to MCU2
 
